b1ef49b4934363b6b53ef032d904b66e8720fcf9
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-06-28 22:02:39 +0530

    ArithmaticOperator.java
    Implement basic arithmetic operations in Numbers class.

---
+++ Operators/ArithmaticOperator.java
@@ -0,0 +1,26 @@
+package operator;
+
+public class Numbers {
+      
+	public static void main(String[]args) {
+		
+		System.out.println(3 + 2); 
+		System.out.println(6 - 2); 
+		System.out.println(6 * 2); 
+		System.out.println(15 / 2); 
+		System.out.println(15 % 2); 
+		System.out.println(15.0 / 2.0); 
+		System.out.println(15.0 % 2.0); 
+  }
+
+}
+
+-----------------------------------------------------------------------------------------------
+Output
+5
+4
+12
+7
+1
+7.5
+1.0
